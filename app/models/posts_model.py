b121138d83ee101d9642986d6f94318a7cff10f1
Mateus Costa <mateusdsilvacosta@outlook.com>
2021-06-14 19:22:22 -0300

    Finalizando Templates

---
+++ app/models/posts_model.py
@@ -0,0 +1,15 @@
+#ok
+from app import db
+from sqlalchemy import Column, Integer, Date, String
+
+
+class KlogPosts(db.Model):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+
+    tittle = Column(String(150), nullable=False)
+    date = Column(Date, nullable=False)
+    contents = Column(String, nullable=False)
+    author = Column(String(150), nullable=False)
+    email = Column(String(150), nullable=False)
--- app/views/blog_posts.py
+++ app/views/blog_posts.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from ..models.posts_model import KlogPosts
+
 
 
 bp = Blueprint("bp_blog_posts", __name__)
@@ -8,13 +11,23 @@ bp = Blueprint("bp_blog_posts", __name__)
 
 @bp.route("/api", methods=['GET'])
 def get_posts():
+    posts = KlogPosts().query.all()
+
+    if posts == []:
+        return render_template("no_content.html")
+
+    return render_template("base.html")
+
+
+
+@bp.route("/api/posts", methods=['GET'])
+def form_for_new_posts():
     pass
 
 
 
-
 @bp.route("/api/posts", methods=['POST'])
-def posts_from_blog():
+def new_posts():
     pass
 
 
@@ -23,3 +36,4 @@ def posts_from_blog():
 @bp.route("/api/<int:post_id>", methods=['GET'])
 def get_one_post(post_id):
     pass
+
